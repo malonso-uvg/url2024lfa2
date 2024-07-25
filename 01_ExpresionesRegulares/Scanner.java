48476874f3e690094dafa50972dfa04a21bcc4bb
Moises Alonso <maalonso@uvg.edu.gt>
2024-07-25 08:16:31 -0600

    Ejemplos ER

---
+++ 01_ExpresionesRegulares/Scanner.java
@@ -0,0 +1,28 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Scanner{
+
+    public static boolean isValidEmailAddress(String email){
+        String usuario = "[a-zA-Z0-9_\\.\\-]+";
+        String dominio = "@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+(\\.[a-zA-Z0-9]+)*";
+        //Pattern pattern = Pattern.compile("^[a-zA-Z0-9_\\.\\-]+@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+(\\.[a-zA-Z0-9]+)*$", Pattern.CASE_INSENSITIVE);
+        Pattern pattern = Pattern.compile("^"+usuario+dominio+"$", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.find();
+    }
+
+    public static boolean isValidURL(String url){
+        Pattern pattern = Pattern.compile("^(https?://)?(www\\.)?[a-zA-Z0-9\\-]+(\\.[a-zA-Z0-9\\-]+)+$", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(url);
+        return matcher.find();
+    }
+
+    public static boolean isValidIP(String ipv4){
+        String octeto = "(1[0-9][0-9])|((2[0-4][0-9])|(25[0-5]))|([0-9]?[0-9])";
+        Pattern pattern = Pattern.compile("^"+octeto+"\\."+octeto+"\\."+octeto+"\\."+octeto+"$", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(ipv4);
+        return matcher.find();
+    }
+
+}
---
+++ 01_ExpresionesRegulares/ScannerTest.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ScannerTest {
+    @Test
+    public void isValidEmailAddressTest() {
+        assertTrue(Scanner.isValidEmailAddress("maalonsog@correo.url.edu.gt"));
+        assertTrue(Scanner.isValidEmailAddress("test.administracion@dominio.gt"));
+        assertTrue(Scanner.isValidEmailAddress("test-administracion@dominio.gt"));
+        assertFalse(Scanner.isValidEmailAddress("test"));
+        assertFalse(Scanner.isValidEmailAddress("test@prueba"));
+    }
+
+    @Test
+    public void isValidURLTest(){
+        assertTrue(Scanner.isValidURL("www.google.c"));
+        assertTrue(Scanner.isValidURL("http://www.google.com"));
+        assertTrue(Scanner.isValidURL("https://www.google.com"));
+        assertTrue(Scanner.isValidURL("url.edu.gt"));
+    }
+
+    @Test
+    public void isValidIPv4Adress(){
+        assertTrue(Scanner.isValidIP("192.168.0.1"));
+        assertTrue(Scanner.isValidIP("200.130.0.255"));
+        assertTrue(Scanner.isValidIP("0.0.0.0"));
+        assertTrue(Scanner.isValidIP("255.255.255.255"));
+        //assertFalse(Scanner.isValidIP("255.256.255.255"));
+        //assertFalse(Scanner.isValidIP("300.1.255.255"));
+        //assertFalse(Scanner.isValidIP("999.1.255.255"));
+        //assertFalse(Scanner.isValidIP("200.1.255"));
+        
+    }
+}
